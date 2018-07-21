817d2b4ea5957c0dacb1456886ef2d5802b79860
Fabrizio Destro <destro.fabrizio@gmail.com>
2018-07-21 19:54:46 +0200

    Added an example of snippet.

---
+++ java/Demo.java
@@ -0,0 +1,15 @@
+package Snippets;
+
+import org.junit.Test;
+
+public class Demo {
+	
+	public static void helloWorld() {
+		System.out.println("Hello World");
+	}
+
+	@Test
+	public void testHelloWorld() {
+		helloWorld();
+	}
+}
