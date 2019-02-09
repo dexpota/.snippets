ab098e37fe0b14ffd67f7c425c32cdc60db93c9a
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-02-09 22:56:32 +0100

    bash: template updated

--- bash/template-script-docopts.bash
+++ bash/template-script-docopts.bash
@@ -1,7 +1,8 @@
 # ---
 # description:
 # language: bash
-# name: template-script
+# name: template-script-docopts
+# version: 0.1
 # ---
 
 #!/usr/bin/env bash
@@ -9,6 +10,9 @@
 # @author
 # @copyright
 # @license
+
+# Configuration
+import_utilites=true
 
 usage() {
 	cat << EOU
@@ -23,7 +27,29 @@ debug() {
 	[ $debug = "true" ]
 }
 
+import() {
+	# if we want to import common utilities
+	if [ "$import_utilites" == true ]; then
+
+		# if an env variable is not defined try a default location
+		if [ -z $SCRIPTING_UTILITIES ]; then
+			scripting_utilities=$HOME/.script/libs/utilities.sh
+		fi
+
+		# Source Scripting Utilities
+		if [ -f "$scripting_utilites" ]; then
+			source "$scripting_utilities"
+		else
+			echo "Scripting utilities not found!"
+			echo "$scripting_utilities doesn't seem to exists."
+			exit -1
+		fi
+	fi
+}
+
 main() {
+	import
+
 	if verbose; then
 		echo "Verbose mode..."
 		echo
