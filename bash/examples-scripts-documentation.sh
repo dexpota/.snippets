d4f7dd0dedb68c56512391020e048d8f0f5e613f
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-05-01 22:48:37 +0200

    bash: how to write docs for bash scripts.

---
+++ bash/examples-scripts-documentation.sh
@@ -0,0 +1,22 @@
+# ---
+# description: This snippets shows how to document your bash script
+# language: bash
+# ---
+
+# Documenting an alias
+
+##
+# git alias - single line description
+#
+# This is the long description and it is a bit longer if you need more info
+# about this code you can write it here
+# @author dexpota
+##
+
+alias g=git
+
+##
+# taskwarrior alias - single line description
+##
+
+alias t=task
